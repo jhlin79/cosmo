e01f46a583c696d6e70e08c0092a34567baa1d90
jhlin79 <jh@Chien-HaoLins-MacBook-Pro.local>
2016-02-03 01:38:25 -0500

    edit twopt

--- twopt/fitsRead.cpp
+++ twopt/fitsRead.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main(int argc, char * argv[])
 {
-  //You can select catalog data here.
-  char expr[] = "Z > 0.43 && Z < 0.7 && WEIGHT_FKP>0";
-  ofstream fout;
-  fout.open(argv[2]);
-  galaxyCatalog cmass(argv[1], expr);
-  fout.precision(8);
-  cmass.save(fout);
-  //If you only want to save 10 galaxy data, try:
-  //cmass.save(fout, 10);
-  fout.flush();
-  fout.close();
- 
-  return 0;
+        //You can select catalog data here.
+        char expr[] = "Z > 0.43 && Z < 0.7";
+        ofstream fout;
+        fout.open(argv[2]);
+        galaxyCatalog cmass(argv[1], expr);
+        fout.precision(8);
+        cmass.save(fout);
+        //If you only want to save 10 galaxy data, try:
+        //cmass.save(fout, 10);
+        fout.flush();
+        fout.close();
+
+        return 0;
 }

31a3648463c3af3f63e1bad777c0bf5f3cef08ee
jhlin79 <jh@Chien-HaoLins-MacBook-Pro.local>
2016-02-03 01:57:59 -0500

    minor

--- twopt/galaxyCatalog.cpp
+++ twopt/galaxyCatalog.cpp
@@ -4,30 +4,30 @@
 #define PI 3.14159265
 
 galaxyCatalog::galaxyCatalog(char *fn, char *expr){
-  //These parameters are for Om=0.274.
-  para[0] = 0.55109459;
-  para[1] = 0.04424543;
-  para[2] = 0.99033654;
-  para[3] = 1.26811119;
-  para[4] = 2.27075546;
-  DH = 3000./0.7;
+        //These parameters are for Om=0.274.
+        para[0] = 0.55109459;
+        para[1] = 0.04424543;
+        para[2] = 0.99033654;
+        para[3] = 1.26811119;
+        para[4] = 2.27075546;
+        DH = 3000./0.7;
 
-  strcpy(redChar, "Z");
-  strcpy(wChar, "WEIGHT_FKP");
-  strcpy(raChar, "RA");
-  strcpy(decChar, "DEC");
+        strcpy(raChar, "RA");
+        strcpy(decChar, "DEC");
+        strcpy(redChar, "Z");
+        strcpy(wChar, "WEIGHT_FKP");
 
-  tptr = new table (fn);
-  tptr->select_rows(expr);
-  n = tptr->get_num_rows();
+        tptr = new table (fn);
+        tptr->select_rows(expr);
+        n = tptr->get_num_rows();
 }
 
 void galaxyCatalog::read(const int& i){
-  //dn < 100
-  tptr->read(raChar,  ra, i, 1);
-  tptr->read(decChar, dec, i, 1);
-  tptr->read(redChar, red, i, 1);
-  tptr->read(wChar, w, i, 1);
+        //dn < 100
+        tptr->read(raChar,  ra, i, 1);
+        tptr->read(decChar, dec, i, 1);
+        tptr->read(redChar, red, i, 1);
+        tptr->read(wChar, w, i, 1);
 
 }
 
@@ -37,41 +37,41 @@ void galaxyCatalog::read(const int& i){
 
 
 void galaxyCatalog::ang2Car(){
-  float sinDec, cosDec, sinRa, cosRa;
-    sinDec = sin(dec[0]*PI/180.0);
-    cosDec = cos(dec[0]*PI/180.0);
-    sinRa  = sin( ra[0]*PI/180.0);
-    cosRa  = cos( ra[0]*PI/180.0);
-    z[0] = red[0] * sinDec;
-    x[0] = red[0] * cosDec * cosRa;
-    y[0] = red[0] * cosDec * sinRa;
- }
- 
+        float sinDec, cosDec, sinRa, cosRa;
+        sinDec = sin(dec[0]*PI/180.0);
+        cosDec = cos(dec[0]*PI/180.0);
+        sinRa  = sin( ra[0]*PI/180.0);
+        cosRa  = cos( ra[0]*PI/180.0);
+        z[0] = red[0] * sinDec;
+        x[0] = red[0] * cosDec * cosRa;
+        y[0] = red[0] * cosDec * sinRa;
+}
+
 void galaxyCatalog::red2Comoving(){
-  float zi;
-  zi = red[0];
-  red[0] = DH * para[2]*zi /sqrt( 1.0 + para[0]*pow(zi, para[3]) + para[1]*pow(zi, para[4]) );
-  
+        float zi;
+        zi = red[0];
+        red[0] = DH * para[2]*zi /sqrt( 1.0 + para[0]*pow(zi, para[3]) + para[1]*pow(zi, para[4]) );
+
 }
 
 void galaxyCatalog::save(std::ofstream &fout){
-  for (int row=1; row<n+1; row++){
-    read(row);
-    red2Comoving();
-    ang2Car();
-    fout << x[0] << " " << y[0] << " " << z[0] << " " << w[0] << std::endl;
-  }
+        for (int row=1; row<n+1; row++) {
+                read(row);
+                red2Comoving();
+                ang2Car();
+                fout << x[0] << " " << y[0] << " " << z[0] << " " << w[0] << std::endl;
+        }
 }
 
 void galaxyCatalog::save(std::ofstream &fout, const int & nmax){
-  for (int row=1; row<nmax; row++){
-    read(row);
-    red2Comoving();
-    ang2Car();
-    fout << x[0] << " " << y[0] << " " << z[0] << " " << w[0] << std::endl;
-  }
+        for (int row=1; row<nmax; row++) {
+                read(row);
+                red2Comoving();
+                ang2Car();
+                fout << x[0] << " " << y[0] << " " << z[0] << " " << w[0] << std::endl;
+        }
 }
 
 galaxyCatalog::~galaxyCatalog(){
-  delete[] tptr;
+        delete[] tptr;
 }
--- twopt/res/readCorrelation.py
+++ twopt/res/readCorrelation.py
@@ -9,17 +9,17 @@ import matplotlib.pyplot as plt
 class pairs:
 
     def __init__(self, ext):
-        f          = open(fn+ext+".dat")        
-        self.ext   = ext
-        self.con   = f.readlines()
-        self.sarr  = np.array( self.con[0].split() )
-        self.muarr = np.array( self.con[1].split() )
-        self.nbins  = len(self.sarr)-1
-        self.nbinmu= len(self.muarr)-1
+        f = open(fn + ext + ".dat")
+        self.ext = ext
+        self.con = f.readlines()
+        self.sarr = np.array(self.con[0].split())
+        self.muarr = np.array(self.con[1].split())
+        self.nbins = len(self.sarr) - 1
+        self.nbinmu = len(self.muarr) - 1
 
-        self.mat   = np.zeros((self.nbins, self.nbinmu))
-        for l in xrange(2, self.nbins+2):
-            self.mat[l-2,:] = np.array( self.con[l].split() )
+        self.mat = np.zeros((self.nbins, self.nbinmu))
+        for l in xrange(2, self.nbins + 2):
+            self.mat[l - 2, :] = np.array(self.con[l].split())
         f.close()
 
         self.sarr = self.sarr.astype(float)
@@ -30,10 +30,11 @@ class pairs:
 ###################################################################
 ##         This class sumarizes all the pair counts.             ##
 ##         It can output correlation functions and figures.      ##
-###################################################################       
+###################################################################
 class twoRes:
+
     def __init__(self, fn):
-        fnorm = open(fn+"-norm.dat")
+        fnorm = open(fn + "-norm.dat")
         conN = fnorm.readlines()
         normD = float(conN[0].split(":")[1])
         normR = float(conN[1].split(":")[1])
@@ -41,21 +42,20 @@ class twoRes:
         self.DD = pairs("-DD")
         self.RR = pairs("-RR")
         self.DR = pairs("-DR")
-        self.factor = normR/normD
+        self.factor = normR / normD
 
     def sbinCenters(self):
         nbins = self.DD.nbins
-        return np.array([ (self.DD.sarr[i] + self.DD.sarr[i+1])/2.0  for i in xrange(nbins)])
+        return np.array([(self.DD.sarr[i] + self.DD.sarr[i + 1]) / 2.0 for i in xrange(nbins)])
 
-
-    ##This function averages out the mu dependence.
+    # This function averages out the mu dependence.
     def xi0(self):
         nbins = self.DD.nbins
-        dd_s = np.array([ sum(self.DD.mat[i,:]) for i in xrange(nbins)])
-        rr_s = np.array([ sum(self.RR.mat[i,:]) for i in xrange(nbins)])
-        dr_s = np.array([ sum(self.DR.mat[i,:]) for i in xrange(nbins)])
-        xi_s = (self.factor**2*dd_s  - self.factor*2*dr_s + rr_s)/rr_s
-        return xi_s/2  ##normalizer: (2l+1)/2
+        dd_s = np.array([sum(self.DD.mat[i, :]) for i in xrange(nbins)])
+        rr_s = np.array([sum(self.RR.mat[i, :]) for i in xrange(nbins)])
+        dr_s = np.array([sum(self.DR.mat[i, :]) for i in xrange(nbins)])
+        xi_s = (self.factor**2 * dd_s - self.factor * 2 * dr_s + rr_s) / rr_s
+        return xi_s / 2  # normalizer: (2l+1)/2
 
     def plot_xi0(self):
         plt.plot(self.sbinCenters(), self.xi0() * self.sbinCenters()**2)
@@ -65,38 +65,37 @@ class twoRes:
         plt.title(fn)
         plt.show()
 
-    ##Transform xi0 to power spectrum
+    # Transform xi0 to power spectrum
     def ps(self, kmax=1.0, n=50):
         sarr = self.sbinCenters()
-        ds = sarr[1]-sarr[0]
+        ds = sarr[1] - sarr[0]
         xi0 = self.xi0()
-        karr = np.linspace(kmax/n, kmax, n)
+        karr = np.linspace(kmax / n, kmax, n)
         pwarr = np.linspace(0, 0, n)
         for i in xrange(n):
             k = karr[i]
-            pwarr[i] = sum(xi0 * sarr * np.sin(k*sarr) * ds /k )
+            pwarr[i] = sum(xi0 * sarr * np.sin(k * sarr) * ds / k)
         return pwarr
 
     def plot_ps(self, type, kmax=0.3, n=100):
-        karr = np.linspace(kmax/n, kmax, n)
+        karr = np.linspace(kmax / n, kmax, n)
         pwarr = self.ps(kmax, n)
-        ##plot k times P(k)
+        # plot k times P(k)
         if type == 'kp':
-            plt.plot(karr, karr*pwarr*np.pi**2/(2*np.pi**2))
+            plt.plot(karr, karr * pwarr * np.pi**2 / (2 * np.pi**2))
             plt.xlabel(r"$k$ (Mpc$^{-1}$)", fontsize=16)
             plt.ylabel(r"$kP(k)/2\pi^2$ (Mpc$^2$)", fontsize=16)
             plt.title("power spectrum")
             plt.show()
-        ##Log plot of the power spectrum.
+        # Log plot of the power spectrum.
         elif type == 'log':
-            plt.plot(np.log10(karr), np.log10(pwarr*np.pi**2))
+            plt.plot(np.log10(karr), np.log10(pwarr * np.pi**2))
             plt.xlabel(r"$\log_{10} k$", fontsize=16)
             plt.ylabel(r"$\log_{10} P(k)$", fontsize=16)
             plt.title("power spectrum")
             plt.xlim([-2.0, -0.5])
             plt.ylim([3.2, 5.1])
             plt.show()
-
 
 
 ################################

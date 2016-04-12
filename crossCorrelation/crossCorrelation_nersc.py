45a72955987fc228130bca156a3a5ab27f03ae86
jhlin79 <jhlin79@gmail.com>
2016-04-12 08:26:11 -0700

    cross

--- crossCorrelation/crossCorrelation_nersc.py
+++ crossCorrelation/crossCorrelation_nersc.py
@@ -7,12 +7,11 @@ import matplotlib.pyplot as plt
 #####################################
 ##            Constants            ##
 #####################################
-fgal =  '/Users/jh/Desktop/data/SDSS/data/galaxy_DR12v5_CMASS_North.fits'
-fgalMask = '/Users/jh/Desktop/data/SDSS/data/mask_DR12v5_CMASS_North.fits'
-fmap ='/Users/jh/Desktop/data/Planck/HFI_SkyMap_857_2048_R2.02_full.fits'
-fmapMaskPlane = '/Users/jh/Desktop/data/Planck/HFI_Mask_GalPlane-apo2_2048_R2.00.fits'
-fmapMaskPS = '/Users/jh/Desktop/data/Planck/HFI_Mask_PointSrc_2048_R2.00.fits'
-NSIDE = 1024
+fgal =  '/global/homes/c/chienhao/data/CMASS/galaxy_DR12v5_CMASS_North.fits'
+fmap ='/global/homes/c/chienhao//data/Planck/HFI_SkyMap_857_2048_R2.02_full.fits'
+fmapMaskPlane = '/global/homes/c/chienhao/data/Planck/HFI_Mask_GalPlane-apo2_2048_R2.00.fits'
+fmapMaskPS = '/global/homes/c/chienhao/data/Planck/HFI_Mask_PointSrc_2048_R2.00.fits'
+NSIDE = 512
 LMAX = 512
 
 #####################################                                                                                                                                            
@@ -35,15 +34,20 @@ galPix = hp.ang2pix(NSIDE, theta, phi)
 
 for pix in galPix:
     galMap[pix] += 1
+    
+galMap_Ngal = len(theta)
+galMap_Npix = 0  ## keep track of the number of pixels covered
+for i in xrange(len(galMap)):
+    if galMap[i] == 0:
+        galMap[i] = hp.UNSEEN
+    else:
+        galMap_Npix+=1
 
-countMean = np.average(galMap)
-galMap = (galMap - countMean)/countMean
+countMean = galMap_Ngal/galMap_Npix
 
-## Galaxy Mask
-#galMask = hp.read_map(fgalMask).astype(bool)
-#galMapMasked = hp.ma(galMap)
-#galMapMasked.mask = np.logical_not(galMask)
-#hp.mollview(galMapMasked, rot=(180,0,0))
+for i in xrange(len(galMap)):
+    if galMap[i] != hp.UNSEEN:
+        galMap[i] = (galMap[i] - countMean)/countMean
 
 
 hp.mollview(galMap, rot=(180,0,0))
@@ -55,29 +59,29 @@ plt.show()
 
 planck857 = hp.read_map(fmap)
 maskPS = hp.read_map(fmapMaskPS, field=5).astype(bool) ##field=5 for F857
-maskPlane = hp.read_map(fmapMaskPlane, field=0).astype(bool) ##field=0 for 20% coverage
-mask = np.logical_and(maskPS, maskPlane)
-planck857Masked = hp.ma(planck857)
-planck857Masked.mask = np.logical_not(mask)
-
-hp.mollview(planck857Masked)
-plt.show()
-
-
+fig = plt.figure()
+maskFields = [0,1,2,3,4,5]
+for f in maskFields:
+    maskPlane = hp.read_map(fmapMaskPlane, field=f).astype(bool) ##field: 20% 40% 60% 70% 80%
+    mask = np.logical_and(maskPS, maskPlane)
+    planck857Masked = hp.ma(planck857)
+    planck857Masked.mask = np.logical_not(mask)
+    hp.mollview(planck857Masked, sub=(len(maskFields),2,2*f+1))
 
 #######################################                                                                                                                                          
 ##    Spherical Harmonics            ##
 #######################################                                                                                                                                          
- 
-cl = hp.anafast(planck857Masked, lmax=LMAX)
-ell = np.arange(len(cl))
-plt.plot(ell, ell * (ell+1) * cl)
-plt.xlabel('$l$')
-plt.ylabel('$l(l+1) C_l$')
-plt.title('Planck 857 GHz auto-power spectrum')
+    plt.subplot(len(maskFields), 2, 2*f+2)
+    cl = hp.anafast(planck857Masked, lmax=LMAX)
+    ell = np.arange(len(cl))
+    plt.plot(ell, ell*(ell+1)*cl)
+    plt.xlabel('$l$')
+    plt.ylabel('$l(l+1) C_l$')
+    plt.title('Planck 857 GHz auto-power spectrum')
+
 plt.show()
 
-c
+
 clGal = hp.anafast(galMap, lmax=LMAX)
 ellGal = np.arange(len(clGal))
 plt.plot(ellGal, clGal)
@@ -86,6 +90,15 @@ plt.xscale('log')
 plt.xlabel('$l$')
 plt.ylabel('$C_l$')
 plt.title('CMASS North dr12 auto-power spectrum')
-plt.xlim([0, LMAX])
-plt.ylim([10**-6, 10**1])
 plt.show()
+
+
+#clCross = hp.anafast(planck857Masked, galMap, lmax = LMAX)
+#ellCross = np.arange(len(clCross))
+#plt.plot(ellCross, clCross)
+##plt.xscale('log')
+##plt.yscale('log')
+#plt.xlabel('$l$')
+#plt.ylabel('$C_l$')
+#plt.title('CMASS North dr12 and Planck 857 GHz cross-power spectrum')
+#plt.show()
